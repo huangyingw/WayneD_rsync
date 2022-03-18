7f317e8adae7af68ee1a51ddd3a0abdf8e7bb124
Ying Huang <huangyingw@gmail.com>
2022-03-17 22:31:27 -0700

    n

--- install.sh
+++ install.sh
@@ -2,3 +2,19 @@
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
+
+apt install -y \
+    acl \
+    attr \
+    autoconf \
+    automake \
+    g++ \
+    gawk \
+    gcc \
+    libacl1-dev \
+    libattr1-dev \
+    liblz4-dev \
+    libssl-dev \
+    libxxhash-dev \
+    libzstd-dev \
+    python3-cmarkgfm
