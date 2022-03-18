88e8914f25532a1943f76b9dacfa0f99ec609345
Ying Huang <huangyingw@gmail.com>
2022-03-17 22:33:53 -0700

    n

---
+++ install_prerequisite.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+
+apt install -y \
+    acl \
+    attr \
+    autoconf \
+    automake \
+    g++ \
+    gawk \
+    gcc \
+    libacl1-dev \
+    libattr1-dev \
+    liblz4-dev \
+    libssl-dev \
+    libxxhash-dev \
+    libzstd-dev \
+    python3-cmarkgfm
