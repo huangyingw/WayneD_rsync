7f2cc4da60c3312e9e45834bb167eb6dde6fd444
Ying Huang <huangyingw@gmail.com>
2022-11-12 16:51:23 -0800

    n

--- do_install.sh
+++ do_install.sh
@@ -3,8 +3,14 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-CPPFLAGS="-I$HOMEBREW_PREFIX/opt/openssl/include/ -I$HOMEBREW_PREFIX/opt/xxhash/include/ -I$HOMEBREW_PREFIX/opt/zstd/include/ -I$HOMEBREW_PREFIX/opt/lz4/include/" \
-    LDFLAGS="-L$HOMEBREW_PREFIX/opt/openssl/lib/ -L$HOMEBREW_PREFIX/opt/xxhash/lib/ -L$HOMEBREW_PREFIX/opt/zstd/lib/ -L$HOMEBREW_PREFIX/opt/lz4/lib/"\
+CPPFLAGS="-I$HOMEBREW_PREFIX/opt/openssl/include/ \
+    -I$HOMEBREW_PREFIX/opt/xxhash/include/ \
+    -I$HOMEBREW_PREFIX/opt/zstd/include/ \
+    -I$HOMEBREW_PREFIX/opt/lz4/include/" \
+    LDFLAGS="-L$HOMEBREW_PREFIX/opt/openssl/lib/ \
+    -L$HOMEBREW_PREFIX/opt/xxhash/lib/ \
+    -L$HOMEBREW_PREFIX/opt/zstd/lib/ \
+    -L$HOMEBREW_PREFIX/opt/lz4/lib/"\
     ./configure \
     --prefix=$HOMEBREW_PREFIX && \
     make reconfigure && \
